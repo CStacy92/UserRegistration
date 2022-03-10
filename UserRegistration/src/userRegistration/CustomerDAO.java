be91f6731d51294c739290523a7c44c233103459
Corey Stacy <stacy.corey1992@gmail.com>
2022-03-10 16:21:08 -0500

    edits made to front end and DAO

--- UserRegistration/src/userRegistration/CustomerDAO.java
+++ UserRegistration/src/userRegistration/CustomerDAO.java
@@ -4,58 +4,74 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CustomerDAO {
-	private static ArrayList <Customer> customers = new ArrayList <Customer>();
+	private static boolean customerExists;
+	private static ArrayList<Customer> customers = new ArrayList<Customer>();
 
 	public static ArrayList<Customer> getCustomers() {
 		return customers;
 	}
-	//Add customer to customer list
+
+	// Add customer to customer list
 	public static void addCustomer(Customer c) {
 		customers.add(c);
 	}
-	
-	//Update customer info
+
+	// Update customer info
 	public static void updatePhone(Customer c) {
 		System.out.println("Enter new phone number for " + c.getfName());
 		Scanner s = new Scanner(System.in);
 		c.setPhoneNumber(s.nextLine());
 		s.close();
 	}
-	
+
 	public static void updateEmail(Customer c) {
 		System.out.println("Enter new email for " + c.getfName());
 		Scanner s = new Scanner(System.in);
 		c.setEmail(s.nextLine());
 		s.close();
 	}
-	
+
 	public static void updateAge(Customer c) {
 		System.out.println("Enter new age for " + c.getfName());
 		Scanner s = new Scanner(System.in);
 		c.setAge(s.nextInt());
 		s.close();
 	}
+
 	public static void updateLname(Customer c) {
 		System.out.println("Enter new last name for " + c.getfName());
 		Scanner s = new Scanner(System.in);
 		c.setLname(s.nextLine());
 		s.close();
 	}
+
 	public static void updateUsername(Customer c) {
 		System.out.println("Enter new username for " + c.getfName());
 		Scanner s = new Scanner(System.in);
 		c.setUserName(s.nextLine());
 		s.close();
 	}
+
 	public static void updateFname(Customer c) {
 		System.out.println("Enter new first name for " + c.getfName());
 		Scanner s = new Scanner(System.in);
 		c.setfName(s.nextLine());
 		s.close();
 	}
+
 	public static void deleteCustomer(Customer c) {
 		customers.remove(c);
 	}
 
+	public static boolean isCustomer(String first, String last) {
+		for (Customer c : customers) {
+			if ((c.getfName() == first) && (c.getLname() == last)) {
+				customerExists = true;
+			} else {
+				customerExists = false;
+			}
+		}
+		return customerExists;
+	}
 
 }
